c9ed8226105803b65aad2bb9e801b52eaab8ebe4
Sayanchowdhury123 <sayanchoudhury506@gmail.com>
2025-07-09 16:17:04 +0530

    h

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router , Routes,Route, useNavigate, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import Signup from './pages/auth/signup'
 import Login from './pages/auth/login'
 import Privateroute from './pages/Privateroute'
@@ -18,7 +18,7 @@ import Editreview from './pages/Editreview'
 import Videoplayer from './pages/videoplayer'
 import Search from './pages/seach'
 import Chatbox from './pages/Chatbox'
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import InstructorLiveStream from './pages/InstructorLiveStream'
 import StudentViewer from './pages/StudentViewer'
 import Courseupdates from './pages/Courseupdate'
@@ -43,54 +43,54 @@ import Homeroute from './pages/Homeroute'
 import Editupdates from './pages/Editupdate'
 
 function App() {
- const {user} = useContext(Authcontext)
+  const { user } = useContext(Authcontext)
 
 
 
   return (
     <>
-    <Router>
-      <Toaster/>
-      <Routes>
-        <Route path='/' element={user ? <Navigate to="/home" /> : <Landing/>} />
-        <Route path='/courses' element={<Courses/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contacts/>} />
-         <Route  path='/Pricing' element={<Pricing/>} />
-        <Route path='/signup' element={<Signup/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/home' element={<Homepage/>} />
-        <Route path='/profile' element={<Privateroute><Profile/></Privateroute>}  />
-        <Route path='/instructor-dasshboard' element={<Privateroute><Instructordashboard/></Privateroute>}  />
-        <Route path='/create-course' element={<Privateroute><Createcourse/></Privateroute>}  />
-        <Route path='/course-management' element={<Privateroute><Cousremanagementpage/></Privateroute>} />
-        <Route  path='/instructor/edit-course/:courseid' element={<Privateroute><Editcourse/></Privateroute>} />
-        <Route  path='/session-lesson/:courseid' element={<Privateroute><Sessionlesson/></Privateroute>} />
-        <Route path='/edit-section/:courseid/:sectionindex' element={<Privateroute><Editsection/></Privateroute>} />
-        <Route path='/edit-lesson/:courseid/:sectionindex/:lessonid' element={<Privateroute><Editlesson/></Privateroute>} />
-        <Route path='/course/:courseid'  element={<Privateroute><Course/></Privateroute>} />
-        <Route path='/reviews/:reviewid/:courseid' element={<Privateroute><Editreview/></Privateroute>} />
-        <Route path='/video/:id' element={<Privateroute><Videoplayer/></Privateroute>} />
-        <Route path='/search' element={<Search/>} />
-        <Route path='/chat/:id' element={<Privateroute><Chatbox/></Privateroute>} />
-        <Route path='/startlive' element={<Privateroute><InstructorLiveStream/></Privateroute>} />
-         <Route path='/joinlive' element={<Privateroute><StudentViewer/></Privateroute>} />
-         <Route path='/course-updates' element={<Privateroute><Courseupdates/></Privateroute>} />
-         <Route path='/addupdate/:courseid' element={<Privateroute><Addupdates/></Privateroute>} />
-         <Route path='*' element={<NotFound/>} />
-         <Route path='/quiz' element={<Privateroute><Quiz/></Privateroute>} />
-         <Route path='/quizes' element={<Privateroute><Quizes/></Privateroute>} />
-         <Route path='/edit-quiz' element={<Privateroute><Editquiz/></Privateroute>} />
-         <Route path='/render-quiz' element={<Privateroute><RenderQuiz/></Privateroute>} />
-         <Route path='/forum/:courseid' element={<Privateroute><Discussionforum/></Privateroute>} />
-         <Route path='/thread/:threadid' element={<Privateroute><Comments/></Privateroute>}  />
-         <Route  path='/buy-course' element={<Privateroute><Buycourse/></Privateroute>} />
-         <Route path='/approval/:courseid' element={<Privateroute><Approval/></Privateroute>} />
-         <Router path='/editupdate/:courseid'  element={<Privateroute><Editupdates/></Privateroute>} />
-      </Routes>
-    </Router>
-       <Back/>
-    
+      <Router>
+        <Toaster />
+        <Routes>
+          <Route path='/' element={user ? <Navigate to="/home" /> : <Landing />} />
+          <Route path='/courses' element={<Courses />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contacts />} />
+          <Route path='/Pricing' element={<Pricing />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<Homepage />} />
+          <Route path='/profile' element={<Privateroute><Profile /></Privateroute>} />
+          <Route path='/instructor-dasshboard' element={<Privateroute><Instructordashboard /></Privateroute>} />
+          <Route path='/create-course' element={<Privateroute><Createcourse /></Privateroute>} />
+          <Route path='/course-management' element={<Privateroute><Cousremanagementpage /></Privateroute>} />
+          <Route path='/instructor/edit-course/:courseid' element={<Privateroute><Editcourse /></Privateroute>} />
+          <Route path='/session-lesson/:courseid' element={<Privateroute><Sessionlesson /></Privateroute>} />
+          <Route path='/edit-section/:courseid/:sectionindex' element={<Privateroute><Editsection /></Privateroute>} />
+          <Route path='/edit-lesson/:courseid/:sectionindex/:lessonid' element={<Privateroute><Editlesson /></Privateroute>} />
+          <Route path='/course/:courseid' element={<Privateroute><Course /></Privateroute>} />
+          <Route path='/reviews/:reviewid/:courseid' element={<Privateroute><Editreview /></Privateroute>} />
+          <Route path='/video/:id' element={<Privateroute><Videoplayer /></Privateroute>} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/chat/:id' element={<Privateroute><Chatbox /></Privateroute>} />
+          <Route path='/startlive' element={<Privateroute><InstructorLiveStream /></Privateroute>} />
+          <Route path='/joinlive' element={<Privateroute><StudentViewer /></Privateroute>} />
+          <Route path='/course-updates' element={<Privateroute><Courseupdates /></Privateroute>} />
+          <Route path='/addupdate/:courseid' element={<Privateroute><Addupdates /></Privateroute>} />
+          <Route path='*' element={<NotFound />} />
+          <Route path='/quiz' element={<Privateroute><Quiz /></Privateroute>} />
+          <Route path='/quizes' element={<Privateroute><Quizes /></Privateroute>} />
+          <Route path='/edit-quiz' element={<Privateroute><Editquiz /></Privateroute>} />
+          <Route path='/render-quiz' element={<Privateroute><RenderQuiz /></Privateroute>} />
+          <Route path='/forum/:courseid' element={<Privateroute><Discussionforum /></Privateroute>} />
+          <Route path='/thread/:threadid' element={<Privateroute><Comments /></Privateroute>} />
+          <Route path='/buy-course' element={<Privateroute><Buycourse /></Privateroute>} />
+          <Route path='/approval/:courseid' element={<Privateroute><Approval /></Privateroute>} />
+          <Route path='/editupdate/:courseid' element={<Privateroute><Editupdates /></Privateroute>} />
+        </Routes>
+      </Router>
+      <Back />
+
     </>
   )
 }
